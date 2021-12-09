005381580162a2cfb6547797b2616faa98c0208b
SivaramShabari <sivaramsbr1@gmail.com>
2021-12-09 13:59:06 +0530

    :tada: Initial commit :sparkles:

---
+++ actions.py
@@ -0,0 +1,32 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+class Teach(Action):
+
+    def name(self) -> Text:
+        return "action_learn"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        subject = next(tracker.get_latest_entity_values("subject"), None)
+        if(subject is None):
+            dispatcher.utter_message(text="I'm sorry, I didn't understand the subject.")
+            return []
+        else:
+            dispatcher.utter_message(text="I'm learning {}".format(subject))
+            return []
